2f757b8cf64fbfacf071806afb05460a1556d7c7
Zambianco <rodrigo.zambianco@yahoo.com.br>
2022-04-24 20:59:58 -0300

    Commit inicial com os arquivos utilizados no PI 1

---
+++ frontend/src/components/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect } from "react-router-dom"
+import HomePage from "./HomePage";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Router>
+            <Switch>
+                <Route exact path="/" component={HomePage}/>
+            </Switch>
+        </Router>
+      </div>
+    );
+  }
+}
+
+const appDiv = document.getElementById("app");
+render(<App />, appDiv);
