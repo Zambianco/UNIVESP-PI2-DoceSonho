2cd895f9b67fba51c8053bc806ce63c46713471a
Renato Zane <renatozane90.rz@gmail.com>
2022-06-18 17:45:50 -0300

    NewUserFormTest

--- DoceSonho/tests.py
+++ DoceSonho/tests.py
@@ -1,63 +1,42 @@
 from django.test import TestCase
 from django.urls import reverse
 import json
+import uuid
 
-from Portfolio.models import Produtos
-from Classificacao.models import Categoria
-
+from DoceSonho.forms import NewUserForm
 
 # Create your tests here.
 
-class ProdutosModelTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        # Set up non-modified objects used by all test methods
-        Categoria.objects.create(nomeCategoria='bolos')
-        Produtos.objects.create(titulo='Bolo de chocolate', descricao='delicioso bolo de chocolate', categoria_id=1)
+class NewUserFormTest(TestCase):
 
-    def test_str_is_titulo(self):
-        produto = Produtos.objects.get(ID=1)
-        expected_object_name = produto.titulo
-        self.assertEquals(expected_object_name, str(produto))
+    def test_valid_email_dot_com(self):
+        form = NewUserForm(data={'username':'teste','email':'teste@teste.com','password1':'1X<ISRUkw+tuK','password2':'1X<ISRUkw+tuK'})
+        print(form.errors,form.non_field_errors)
+        self.assertTrue(form.is_valid())
 
+    def test_valid_email_dot_com_dot_br(self):
+        form = NewUserForm(data={'username':'teste','email':'teste@teste.com.br','password1':'1X<ISRUkw+tuK','password2':'1X<ISRUkw+tuK'})
+        print(form.errors,form.non_field_errors)
+        self.assertTrue(form.is_valid())
 
-    def test_titulo_max_lenth(self):
-        produto = Produtos.objects.get(ID=1)
-        max_length = produto._meta.get_field('titulo').max_length
-        self.assertEquals(max_length, 200)
+    def test_invalid_email_without_dot_com(self):
+        form = NewUserForm(data={'username':'teste','email':'teste@teste','password1':'1X<ISRUkw+tuK','password2':'1X<ISRUkw+tuK'})
+        print(form.errors,form.non_field_errors)
+        self.assertFalse(form.is_valid())       
 
-class ProdutosViewTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        # Create 13 authors for pagination tests
+    def test_invalid_email_without_dot_arroba(self):
+        form = NewUserForm(data={'username':'teste','email':'teste.teste.com.br','password1':'1X<ISRUkw+tuK','password2':'1X<ISRUkw+tuK'})
+        print(form.errors,form.non_field_errors)
+        self.assertFalse(form.is_valid())   
 
-        Categoria.objects.create(nomeCategoria='bolos')
-        Categoria.objects.create(nomeCategoria='doces')
+    def test_invalid_password_match(self):
+        form = NewUserForm(data={'username':'teste','email':'teste@teste.com.br','password1':'1X<ISRUkw+tuK','password2':'1X<ISRUkw+tuf'})
+        print(form.errors,form.non_field_errors)
+        self.assertFalse(form.is_valid())             
 
-        number_produtos = 10
+    def test_invalid_password_complexity(self):
+        form = NewUserForm(data={'username':'teste','email':'teste@teste.com.br','password1':'123456789','password2':'123456789'})
+        print(form.errors,form.non_field_errors)
+        self.assertFalse(form.is_valid())        
 
-        for produto_id in range(number_produtos):
-                Produtos.objects.create(
-                titulo=f'Bolo {produto_id}',
-                descricao=f'Bolo {produto_id}',
-                categoria_id=1
-        )
-
-        for produto_id in range(number_produtos):
-            Produtos.objects.create(
-                titulo=f'Doce {produto_id}',
-                descricao=f'Doce {produto_id}',
-                categoria_id=2
-        )
-
-    def test_list_produtos(self):
-        response = self.client.get('/listProdutos/')
-        self.assertEqual(response.status_code, 200)
-        produtoList = json.loads(response.content)
-        self.assertEqual(len(produtoList),20)
-
-    def test_list_categorias(self):
-        response = self.client.get('/listCategoria/')
-        self.assertEqual(response.status_code, 200)
-        categoriaList = json.loads(response.content)
-        self.assertEqual(len(categoriaList),2)
+        
